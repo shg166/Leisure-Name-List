bb6bdcec3d764ac93b6cf711ff4e6dcb4676eaaf
adon <86583577+shg166@users.noreply.github.com>
2023-06-23 15:30:18 +0900

    Add Code & Name List

---
+++ Mod.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+using UnityEngine;
+using ColossalFramework.UI;
+using ICities;
+
+namespace JP_LeisureNameList
+{
+	public class JP_LeisureNameList : IUserMod
+	{
+
+		public string Name
+		{
+			get { return "JP Leisure Name List (CNL) v1.0"; }
+		}
+
+		public string Description
+		{
+			get { return "Custom Name Listsを使って、DLC Parklifeのパークエリア名を変更します"; }
+		}
+
+	}
+}
